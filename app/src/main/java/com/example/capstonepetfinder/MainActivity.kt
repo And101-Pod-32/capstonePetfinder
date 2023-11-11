26ac979e701dd3135b02ddd22a09de075fb2873f
kipkorir <kbrandon98@proton.me>
2023-11-11 13:32:49 -0500

    Added functionality to the spinners

--- app/src/main/java/com/example/capstonepetfinder/MainActivity.kt
+++ app/src/main/java/com/example/capstonepetfinder/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.capstonepetfinder
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val genderSpinner : Spinner = findViewById(R.id.gender)
+        val typeSpinner : Spinner = findViewById(R.id.type)
+        val sizeSpinner : Spinner = findViewById(R.id.size)
+
+        spinnerAdapter(genderSpinner, R.array.gender_options)
+        spinnerAdapter(sizeSpinner, R.array.size_options)
+        spinnerAdapter(typeSpinner, R.array.type_options)
+    }
+
+    private fun spinnerAdapter(spinner: Spinner, spinnerOptions: Int) {
+        ArrayAdapter.createFromResource(
+            this,
+            spinnerOptions,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = adapter
+        }
     }
 }
