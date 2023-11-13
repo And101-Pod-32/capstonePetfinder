be3334e1df2f2cb88c1ed78b97355f50f03110b2
availe <94802525+availe@users.noreply.github.com>
2023-11-12 22:52:21 -0500

    xlm and rootbinding

--- app/src/main/java/com/example/capstonepetfinder/MainActivity.kt
+++ app/src/main/java/com/example/capstonepetfinder/MainActivity.kt
@@ -4,9 +4,12 @@ import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
+import com.example.capstonepetfinder.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
 
     // creating a list for images
     private lateinit var imageList:MutableList<String>
@@ -14,15 +17,14 @@ class MainActivity : AppCompatActivity() {
     private lateinit var sexList:MutableList<String>
     private lateinit var breedList:MutableList<String>
 
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val genderSpinner : Spinner = findViewById(R.id.gender)
-        val typeSpinner : Spinner = findViewById(R.id.type)
-        val sizeSpinner : Spinner = findViewById(R.id.size)
+        val genderSpinner : Spinner = binding.spinnerGender
+        val typeSpinner : Spinner = binding.spinnerType
+        val sizeSpinner : Spinner = binding.spinnerSize
 
 
         //initializing the lists
